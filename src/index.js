8b823cf72bfb15daaa441fdea0523bbe8171d748
Hector Lopez <hlopezcarballo@gmail.com>
2021-03-25 03:48:57 -0600

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./components/HelloWorld";
+import "./assets/styles/main.styl";
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
