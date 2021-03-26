3a5cd89d0d629461c2dcef908292caf2023d7146
Hector Lopez <hlopezcarballo@gmail.com>
2021-03-26 02:37:06 -0600

    Converted HTML and CSS into react components

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "../assets/styles/App.scss";
-import Header from "../components/Header";
-import Search from "../components/Search.jsx";
+
 import Categories from "../components/Categories.jsx";
 import Carousel from "../components/Carousel.jsx";
 import CarouselItem from "../components/CarouselItem.jsx";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Search from "../components/Search.jsx";
+
+import "../assets/styles/App.scss";
 
 const App = () => {
   return (
@@ -14,8 +17,36 @@ const App = () => {
       <Categories>
         <Carousel>
           <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
         </Carousel>
       </Categories>
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
     </>
   );
 };
